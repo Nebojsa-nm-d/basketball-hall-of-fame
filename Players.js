9cc27699ba4aa6844cd7f14b41ece618ec24de04
nebojsa-mitrovic <32269043+Nebojsa-nm-d@users.noreply.github.com>
2020-12-18 16:09:37 +0100

    Creating Players JSON

---
+++ Players.js
@@ -0,0 +1,37 @@
+const players = [
+	{
+		id: "michael-jordan",
+		name: "Michael Jordan",
+		points: 32292,
+		assists: 5633,
+		rebounds: 6672,
+		steals: 2514,
+		blocks: 893,
+		nbaTitles: "6x (1991, 1992, 1993, 1996, 1997, 1998)",
+		mvpTitles: "5x (1988, 1991, 1992, 1996, 1998)",
+	},
+	{
+		id: "hakeem-olajuwon",
+		name: "Hakeem Olajuwon",
+		points: 26946,
+		assists: 3058,
+		rebounds: 13748,
+		steals: 2162,
+		blocks: 3830,
+		nbaTitles: "2x (1994, 1995)",
+		mvpTitles: "1x (1994)",
+	},
+	{
+		id: "kobe-bryant",
+		name: "Kobe Bryant",
+		points: 33643,
+		assists: 6306,
+		rebounds: 7047,
+		steals: 1944,
+		blocks: 640,
+		nbaTitles: "5× (2000–2002, 2009, 2010)",
+		mvpTitles: "1x (2008)",
+	},
+];
+
+module.exports = players;
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
+const players = require("./Players");
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
-});
+app.get("/api/players", (req, res) => res.json(players));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Move to seperate config file
 const PORT = process.env.PORT || 5000;
