b552cc18dc75f82b46636f209c3d19c434c86441
nebojsa-mitrovic <32269043+Nebojsa-nm-d@users.noreply.github.com>
2020-12-21 07:51:05 +0100

    Getting Single Player with ID

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const players = require("./Players");
 
 const app = express();
 
+// Get all players
 app.get("/api/players", (req, res) => res.json(players));
 
+// Get single players
+app.get("/api/players/:id", (req, res) => {
+	const found = players.some((player) => player.id === req.params.id);
+
+	if (found) {
+		res.json(players.filter((player) => player.id === req.params.id));
+	} else {
+		res.status(400).json({ message: "This player is not in Hall of Fame" });
+	}
+});
+
+// Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Move to seperate config file
